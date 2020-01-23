e95100ceabbea76a9e62123a0622ef33eda7a7da
Shiwali <45665313+pandeyshivali@users.noreply.github.com>
2020-01-23 10:33:19 +0530

    Create Deleting_from_array.cpp
    This program is for deleting an element from a desired index of the array.

---
+++ Deleting_from_array.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class array{
+    public:
+int arr[100],n;
+void getdata(){
+cout<<"enter the number of elements "<<endl;
+cin>>n;
+cout<<"enter the elements in the array: "<<endl;
+for(int i=1;i<=n;i++){
+    cin>>arr[i];
+}
+cout<<"array elements are :";
+for(int i=1;i<=n;i++){
+    cout<<arr[i]<<"\t";
+}
+}
+int pos;
+void deletion(){
+cout<<endl<<"enter position from which you want to delete the element "<<endl;
+cin>>pos;
+for(int i=pos;i<=n;i++){
+    arr[i]=arr[i+1];
+}
+n=n-1;
+cout<<"new array elements are :"<<endl;
+for(int i=1;i<=n;i++){
+    cout<<arr[i]<<"\t";
+}
+}
+};
+int main(){
+array a;
+a.getdata();
+a.deletion();
+return 0;
+}
